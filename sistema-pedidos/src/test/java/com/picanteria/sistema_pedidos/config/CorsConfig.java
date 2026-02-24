47c8777334899cf7a4b8a0a5eed77bb101074701
Hilton14-g <rivasgarciahilton@gmail.com>
2026-02-23 19:22:29 -0500

    Agregado CORS para acceso externo

---
+++ sistema-pedidos/src/test/java/com/picanteria/sistema_pedidos/config/CorsConfig.java
@@ -0,0 +1,23 @@
+package com.picanteria.sistema_pedidos.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class CorsConfig {
+
+    @Bean
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/**")
+                        .allowedOrigins("*") // Permite que el QR y el celular entren sin bloqueos
+                        .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
+                        .allowedHeaders("*");
+            }
+        };
+    }
+}
